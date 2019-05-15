0d825da8395ac9c0ae764fabfb8b629a65980514
D Smith <dsmith82@uw.edu>
2019-05-14 20:09:07 -0700

    Add Login/Logout page header

--- blogging/views.py
+++ blogging/views.py
@@ -16,10 +16,11 @@ def stub_view(request, *args, **kwargs):
 def list_view(request):
     published = Post.objects.exclude(published_date__exact=None)
     posts = published.order_by('-published_date')
-    template = loader.get_template('blogging/list.html')
+    # template = loader.get_template('blogging/list.html')
     context = {'posts': posts}
-    body = template.render(context)
-    return HttpResponse(body, content_type="text/html")
+    # body = template.render(context)
+    # return HttpResponse(body, content_type="text/html")
+    return render(request, 'blogging/list.html', context)
 
 def detail_view(request, post_id):
     published = Post.objects.exclude(published_date__exact=None)
